7563659f56ac819da90ee0f54be1f194ba2a2c80
Parlad Dhungana <W0302667@campus.nscc.ca>
2018-01-10 15:58:18 -0400

    commint and push of the first app

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -16,6 +16,7 @@ namespace FirstConsoleApp
             {
 
                 Console.WriteLine("Runnig as server...");
+                Program.PressedKey();
             }
             else
             {
