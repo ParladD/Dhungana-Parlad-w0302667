64aecf79aed13b53ce709ee44e41c51956bdb968
Parlad Dhungana <W0302667@campus.nscc.ca>
2018-01-30 15:05:52 -0400

    Done StatusCode DataLayer

---
+++ BugTracker/BugTrackerDataLayer/StatusCodes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackerDataLayer
+{
+    class StatusCodes
+    {
+
+        public List <StatusCode> GetStatusCodeList()
+        {
+            List<StatusCode> statusCodes = new List<StatusCode>();
+
+            using (SqlConnection connection = DB.GetSqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"GetStatusCode";
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        StatusCode statusCode = new StatusCode();
+                        statusCode.LoadStatusCode(reader);
+                        statusCodes.Add(statusCode);
+                    }
+
+                }//end using sqlcommand
+
+            
+            }//end using sql conneciton
+
+            return statusCodes;
+        }
+
+
+    }//end StatusCodesclass
+
+
+
+    public class StatusCode {
+
+        public string StatusCodeDescription { get; set;}
+
+        public void LoadStatusCode(SqlDataReader reader)
+        {
+            StatusCodeDescription = reader["StatusCodeDesc"].ToString();
+        }
+
+    }
+
+}//end namespace
